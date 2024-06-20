0e565eba6694848cfd45b903e311dc51ed1000b0
raja226 <iosrajasekhar@gmail.com>
2024-06-20 13:18:05 +0530

    initial commit

---
+++ testjavascript.js
@@ -0,0 +1,21 @@
+console.log("First JS program Rajasekhar");
+//Variable Decalartion:
+
+var a= 10 
+var b= "Test"
+var c = true
+console.log(a);
+console.log(b);
+
+console.log(c);
+
+var myName = "Rajasekhar",age = 35, designation= "Developer";
+
+console.log("Name: " +myName +"\nand Age: "+ age +"\ndesignation: " +designation);
+
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(a));
+console.log(a**2);//a power 2
+console.log(a**3); //a power of 3
+
