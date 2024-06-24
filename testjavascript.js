23561f46e766847e0c7fdaa07ae66b0ebac23215
raja226 <iosrajasekhar@gmail.com>
2024-06-24 14:01:40 +0530

    just learning forloop

--- testjavascript.js
+++ testjavascript.js
@@ -1,3 +1,6 @@
+//"use strict"; // Enable strict mode
+
+
 console.log("First JS program Rajasekhar");
 //Variable Decalartion:
 
@@ -19,3 +22,41 @@ console.log(typeof(a));
 console.log(a**2);//a power 2
 console.log(a**3); //a power of 3
 
+var val1= 100;
+var val2 = 200;
+var val3 = "100";
+console.log(val2> val1);
+
+console.log(val1 == val3);
+console.log(val1 === val3);
+
+//type conversion : implicit,explicit conversion
+
+console.log("hi"+"hello");
+console.log("hi" - "hello"); //NaN
+
+console.log(100 + null);
+console.log(100 *null);
+
+const num = "100.0";
+console.log(typeof Number(num));
+console.log( parseFloat(num));
+let date = new Date();
+console.log(date);
+console.log(Number(date));
+console.log(date.getTime());
+console.log(date.toString());
+
+//use strict : avoid the with out datat type of variable declaration.
+withStrictfunction();
+
+
+
+function withStrictfunction()
+{
+   // "use strict"; 
+
+basicval = "test" // invalid
+
+console.log("basic value:"+basicval);
+}
